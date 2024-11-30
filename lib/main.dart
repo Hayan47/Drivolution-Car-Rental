d31e4448cba4b4b96fad31c026629799f8f95dd2
unknown <hayan.bouobaid47@gmail.com>
2024-11-30 15:53:30 +0300

    Added Internet Checker

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:drivolution/app_router.dart';
+import 'package:drivolution/logic/internet_cubit/internet_cubit.dart';
+import 'package:drivolution/presentation/themes/app_colors.dart';
 import 'package:drivolution/presentation/themes/themes.dart';
+import 'package:drivolution/presentation/widgets/toast.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
@@ -24,10 +28,40 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: appRouter.onGenerateRoute,
-      theme: CustomTheme.appTheme,
+    return BlocProvider(
+      create: (context) => InternetCubit(),
+      child: MaterialApp(
+        builder: (context, child) => Overlay(
+          initialEntries: [
+            OverlayEntry(
+              builder: (context) => BlocListener<InternetCubit, InternetState>(
+                listenWhen: (previous, current) => previous != current,
+                listener: (context, state) {
+                  if (state is InternetConnected) {
+                    showToastMessage(
+                      context,
+                      'Internet Connection Restored',
+                      const Icon(Icons.done,
+                          color: AppColors.successGreen, size: 18),
+                    );
+                  } else {
+                    showToastMessage(
+                      context,
+                      'No Internet Connection',
+                      const Icon(Icons.error,
+                          color: AppColors.alertRed, size: 18),
+                    );
+                  }
+                },
+                child: child,
+              ),
+            ),
+          ],
+        ),
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: appRouter.onGenerateRoute,
+        theme: CustomTheme.appTheme,
+      ),
     );
   }
 
